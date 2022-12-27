eb7bf8f2305bcd5f53e9ff12a7ab5f1d07c34ca3
lytem <lytem85@gmail.com>
2022-12-27 19:17:33 +0100

    rename test laravel

---
+++ mycity/resources/views/product/create.blade.php
@@ -0,0 +1,56 @@
+@extends('layouts.app')
+@section('content')
+    <div class="container-fluid">
+
+        <form action="{{ route('products.store') }}" method="post">
+         @csrf
+        <div class="card">
+
+            <div class="card-header">
+                <div class="float-left">
+                   <h2>New Product</h2>
+                </div>
+                <div class="float-right"><button class="btn btn-success"><i class="fa fa-floppy-o"
+                            aria-hidden="true"></i></button></div>
+
+            </div>
+
+            <div class="card-body">
+
+                    <div class="">
+                        <label class="required mb-2">Title</label>
+
+                        <input type="text" name="product[title]" value="{{ old('product.title') }}"
+                            class="form-control @error('product.title') is-invalid @enderror" placeholder="Title" />
+                         @error('product.title')
+                      <span class="text-danger">{{ $errors->first('product.title') }}</span>
+                  @enderror
+                    </div>
+                    <div class="mt-4">
+                        <label class="required mb-2">Price</label>
+                        <input type="number" name="product[price]" value="{{ old('product.price') }}" min="0" step="any"
+                            class="form-control @error('product.price') is-invalid @enderror" placeholder="Price" />
+                         @error('product.price')
+                      <span class="text-danger">{{ $errors->first('product.price') }}</span>
+                  @enderror
+                    </div>
+                    <div class="mt-4">
+                        <label class="required mb-2">Publish Date</label>
+                        <input type="date" name="product[publish_date]" value="{{ old('product.publish_date') }}"
+                            class="form-control  @error('product.publish_date') is-invalid @enderror" placeholder="" />
+                        @error('product.publish_date')
+                      <span class="text-danger">{{ $errors->first('product.publish_date') }}</span>
+                  @enderror
+                    </div>
+
+            </div>
+
+            <div class="card-footer">
+                <a href="{{ url()->previous() }}" class="btn btn-secondary">Indietro</a>
+            </div>
+
+        </div>
+
+    </form>
+    </div>
+@endsection
